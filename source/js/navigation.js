2378c9108831ec1be71763aec5cbb92da6c5d28d
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-01-01 23:45:07 +0100

    Начала ПП стилизацию мобильной версии главной страницы, реализовала функционал мобильной навигации

---
+++ source/js/navigation.js
@@ -0,0 +1,11 @@
+var navigationList = document.querySelector(".nav__menu-list");
+var navigationToggler = document.querySelector(".nav__menu-toggler");
+
+navigationToggler.classList.remove("nav__menu-toggler--no-js");
+navigationToggler.classList.remove("nav__menu-toggler--active");
+navigationList.classList.remove("nav__menu-list--active");
+
+navigationToggler.addEventListener("click", function(){
+  navigationToggler.classList.toggle("nav__menu-toggler--active");
+  navigationList.classList.toggle("nav__menu-list--active");
+});
